836eff2f7e0324ba295d560456c9eb71e3ac10fe
Kenneth Murray <kwmwins@gmail.com>
2024-08-18 17:04:15 -0700

    Create inputDataProcessor.py

---
+++ inputDataProcessor.py
@@ -0,0 +1,16 @@
+'''Task 1: Input Length Validator Write a script that asks for and checks the length of the user's 
+first name and last name. Both should be at least 2 characters long. If not, print an error message.
+'''
+def input_length_validator():
+
+    first_name = input("Enter your first name: ")
+    last_name = input("Enter your last name: ")
+
+    if len(first_name) < 2:
+        print("Name must be at least 2 characters long. ")
+    elif len(last_name) < 2: 
+        print("Name must be at least 2 characters long. ")
+    else:
+        print("Valid username.")
+        
+input_length_validator()
